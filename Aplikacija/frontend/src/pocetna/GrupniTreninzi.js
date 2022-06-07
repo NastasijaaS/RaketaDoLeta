f0581e2f7b0f7506d5ad169dcd2d8108fb32722d
nadajovanovic <nada4jovanovic@gmail.com>
2022-06-07 02:53:54 +0200

    grupnitreninzi-f

--- Aplikacija/frontend/src/pocetna/GrupniTreninzi.js
+++ Aplikacija/frontend/src/pocetna/GrupniTreninzi.js
@@ -2,15 +2,20 @@ import '../styles/usluge.css'
 import { useState, useEffect } from 'react'
 import { GetData } from '../komponente/Fetch'
 import CircularProgress from '@mui/material/CircularProgress';
+import { Card, CardActions, CardContent, CardMedia, Grid, Typography, Button } from '@mui/material';
+
 
 const GrupniTreninzi = () => {
     const [treninzi, setTreninzi] = useState([])
     const [greska, setGreska] = useState(false)
     const [isLoading, setIsLoading] = useState(false)
 
+
+
     useEffect(() => {
         GetData("http://localhost:8800/api/korisnik/vidiGrupneTreninge", setTreninzi, setGreska, setIsLoading)
     }, [])
+
 
     return (
         <div className="sveUsluge">
@@ -18,15 +23,40 @@ const GrupniTreninzi = () => {
 
             {greska && <p className='greska'>Doslo je do greske prilikom ucitavanja</p>}
 
-            {treninzi.map((tr) => (
-                <div key={tr._id} >
+            {treninzi.map((tr, i) => (
+                 <Card key = {i}  sx = {{marginBottom: '5%'}}>
+                    <Grid container >
+                        <Grid item xs={12} sm={4}>
+                            <CardContent>
+                                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                                Usluga
+                                </Typography>
+                                <Typography variant="h5" component="div">
+                                    {tr.nazivGrupnogTreninga}
+                                </Typography>
+                                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                                {tr.imeT} {tr.prezimeT}
+                                </Typography>
+                                <Typography variant="body2">
+                                Intenzitet trening: {tr.intenzitet}
+                                </Typography>
+                            </CardContent>
+                        </Grid>
+                        <Grid item xs={12} sm={8} className = "zakazi">
+                            <CardActions >
+                                <Button fullWidth variant="contained" size="small">Zakazite termin</Button>
+                            </CardActions>
+                        </Grid>
+                    </Grid>
+                </Card>
 
-                    <div className="usluga">
-                        <span className="nazivUsluge">{tr.nazivGrupnogTreninga}</span>
-                        <span className="cenaUsluge">{tr.datum}</span> 
-                    </div>
 
-                </div>
+                //   {/* 
+                //     <div className="usluga">
+                //         <span className="nazivUsluge">{tr.nazivGrupnogTreninga}</span>
+                //         <span className="cenaUsluge">{tr.datum}</span> 
+                //     </div> */}
+
             ))}
 
         </div >
