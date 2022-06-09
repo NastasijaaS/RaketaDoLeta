df7281155ebb982cd9ca5008a9f6409e533a694d
unknown <milicaj@elfak.rs>
2022-06-09 02:34:11 +0200

    ruta

--- Aplikacija/frontend/src/App.js
+++ Aplikacija/frontend/src/App.js
@@ -8,14 +8,14 @@ import Blog from './pocetna/Blog'
 import Register from './pocetna/RegisterForma';
 import ScrollToTop from './komponente/ScrollToTop';
 import UserPocetna from './pocetna/UserPocetna';
-import React, { createContext, useContext, useMemo, useState} from "react";
+import React, { createContext, useContext, useMemo, useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate, NavigationType } from "react-router-dom";
 import { UserContext } from './context/UserContext';
 import { CssBaseline, IconButton } from '@mui/material';
 import Navbar from './komponente/Navbar';
 import Uprava from './profil/ProfilUprava'
 import Footer from './komponente/Footer';
-import { useTheme, ThemeProvider,createTheme} from '@mui/material/styles';
+import { useTheme, ThemeProvider, createTheme } from '@mui/material/styles';
 import ZakazaniTreninzi from './profil/Korisnik/ZakazaniTreninzi';
 import Napredak from './profil/Korisnik/Napredak';
 import GrupniTreninzi from './pocetna/GrupniTreninzi';
@@ -47,101 +47,101 @@ const darkTheme = createTheme({
   },
 });
 
-const ColorModeContext = createContext({ toggleColorMode: () => {} });
+const ColorModeContext = createContext({ toggleColorMode: () => { } });
 
 function App() {
 
   const [mode, setMode] = useState(false);
 
-  const theme = 
-      createTheme({
-        palette: {
-          mode: mode ? "dark" : "light"
-        }
-      })
+  const theme =
+    createTheme({
+      palette: {
+        mode: mode ? "dark" : "light"
+      }
+    })
 
 
   {/*const theme = useTheme();
     const colorMode = useContext(ColorModeContext);*/}
-    const { user } = useContext(UserContext);
+  const { user } = useContext(UserContext);
 
   return (
-     <> 
-     <ThemeProvider theme={darkTheme}> 
-      <CssBaseline />
-      <Router>
-        <ScrollToTop enableColorScheme />
+    <>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <Router>
+          <ScrollToTop enableColorScheme />
 
-        <Navbar check = {mode} change = {()=>setMode(!mode)}/>
-        {/* {
+          <Navbar check={mode} change={() => setMode(!mode)} />
+          {/* {
         (user && user.tip === 'Uprava' && <NavbarUprava />)
          ||
         ((user && user.tip === 'Trener') ? (<NavbarTrener />) : <Navbar check = {mode} change = {()=>setMode(!mode)}/>)
         } */}
-        
-        {/* {theme.palette.mode} mode
+
+          {/* {theme.palette.mode} mode
       <IconButton sx={{ ml: 1 }} onClick={colorMode.toggleColorMode} color="inherit">
         {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
       </IconButton> */}
 
 
-        <Routes>
+          <Routes>
 
-          <Route path='/' element={<Pocetna />}>
-          </Route>
+            <Route path='/' element={<Pocetna />}>
+            </Route>
 
-          <Route path='/pocetna' element={<Pocetna />}>
-          </Route>
+            <Route path='/pocetna' element={<Pocetna />}>
+            </Route>
 
-          <Route path='/treneri' element={<Treneri />}>
-          </Route>
+            <Route path='/treneri' element={<Treneri />}>
+            </Route>
 
-          <Route path='/onama' element={<Onama />}>
-          </Route>
+            <Route path='/onama' element={<Onama />}>
+            </Route>
 
-          <Route path='/usluge' element={<Usluge />}>
-          </Route>
+            <Route path='/usluge' element={<Usluge />}>
+            </Route>
 
-          <Route path='/blog' element={<Blog />} />
+            <Route path='/blog' element={<Blog />} />
 
-          <Route path='/blog/:tag/:naslov' element={<VelikiBlog />} />
-
-
-          <Route path='/login' element=
-            {user ? <Navigate replace to="/profil" /> : <LogIn />}>
-          </Route>
-
-          <Route path='/signup' element=
-            {user ? <Navigate replace to="/profil" /> : <Register />}>
-          </Route>
-
-          {/* <Route path='/signup' element={<Register />} /> */}
-
-          <Route path='/registracija/podaci' element={<Zelje />} />
-
-          <Route path='/profil' element=
-            {!user ? <Navigate replace to="/pocetna" /> : <UserPocetna />}>
-          </Route>
-
-          {/* <Route path='/RDL/uprava/:username' element={<Uprava />} /> */}
-
-          <Route path='/napredak' element={<Napredak />} />
-          <Route path='/vasitreninzi' element={<ZakazaniTreninzi />} />
-
-          <Route path='/grupnitreninzi' element={<GrupniTreninzi />} />
-
-          {/* <Route path='/profil' element={<UserPocetna />} ></Route> */}
-
-          <Route path='/RDL/trener/korisnici' element={<KorisniciTrenera />} />
-          <Route path='/RDL/trener/:username/zahtevi' element={<ZahteviTrenera />} />
-          <Route path='/RDL/trener/:username/treninzi' element={<TreninziTrenera />} />
+            <Route path='/blog/:tag/:naslov' element={<VelikiBlog />} />
 
 
-        </Routes>
+            <Route path='/login' element=
+              {user ? <Navigate replace to="/profil" /> : <LogIn />}>
+            </Route>
 
-        {/* <Footer /> */}
-      </Router>
-     </ThemeProvider >
+            <Route path='/signup' element=
+              {user ? <Navigate replace to="/profil" /> : <Register />}>
+            </Route>
+
+            {/* <Route path='/signup' element={<Register />} /> */}
+
+            <Route path='/registracija/podaci' element={<Zelje />} />
+
+            <Route path='/profil' element=
+              {!user ? <Navigate replace to="/pocetna" /> : <UserPocetna />}>
+            </Route>
+
+            {/* <Route path='/RDL/uprava/:username' element={<Uprava />} /> */}
+
+            <Route path='/napredak' element={<Napredak />} />
+            <Route path='/vasitreninzi' element={<ZakazaniTreninzi />} />
+
+            <Route path='/grupnitreninzi' element={<GrupniTreninzi />} />
+
+            {/* <Route path='/profil' element={<UserPocetna />} ></Route> */}
+
+            <Route path='/RDL/trener/korisnici' element={<KorisniciTrenera />} />
+            <Route path='/RDL/trener/:username/zahtevi' element={<ZahteviTrenera />} />
+            <Route path='/RDL/trener/:username/treninzi' element={<TreninziTrenera />} />
+            <Route path='/trener/korisnik/:ime' element={<KorisnikVeliko />} />
+
+          </Routes>
+
+          {/* <Footer /> */}
+        </Router>
+      </ThemeProvider >
 
     </>
   );
@@ -177,4 +177,4 @@ function App() {
 //   );
 // }
 
- export default App;
+export default App;
